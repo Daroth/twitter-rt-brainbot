29f219af702c261c2bb48f589141f6cd6d5c5267
debaser <debaser@golgoth.(none)>
2012-08-15 16:50:24 +0200

    first unetested version

---
+++ sample-config.js
@@ -0,0 +1,13 @@
+{
+    "twitter": {
+        "consumer_key": "",
+        "consumer_secret": "",
+        "access_token_key": "-y2NhsJapiyDH4wqb025R7mMxyZgMSIrBNdeGl754",
+        "access_token_secret": ""
+    },
+    "accounts": [
+      "twitter", "BraindeadCrew", ... (any account to watch)
+    ],
+    "hashtag": "braindead",
+    "myself": "your username"
+}
